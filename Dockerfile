9405491bf9b82935a60befa6815ad6df44652bd1
Oliver Philipp Noack <oliver.noack@diverseco.com.au>
2020-01-14 11:13:50 +1030

    Finalised working Docker image.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM ubuntu
 RUN apt-get update; apt-get install --no-install-recommends -y vlc-bin vlc-plugin-base; apt-get clean; apt-get autoclean
-COPY rtsp-to-mjpeg.sh /
-RUN chmod 755 /rtsp-to-mjpeg.sh
-USER daemon
-ENTRYPOINT [ "/rtsp-to-mjpeg.sh" ]
+RUN useradd -ms /bin/bash streamer
+WORKDIR /home/streamer
+COPY rtsp-to-mjpeg.sh /home/streamer/
+USER streamer
+ENTRYPOINT [ "/home/streamer/rtsp-to-mjpeg.sh" ]
 EXPOSE 8080
--- rtsp-to-mjpeg.sh
+++ rtsp-to-mjpeg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/bin/vlc --intf dummy rtsp://$CAMERAUSER:$CAMERPASSWORD@$CAMERAIP:$CAMERAPORT//$CAMERASTREAM --sout '#transcode{vcodec=MJPG,venc=ffmpeg{strict=1}}:standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8080/}'
+/usr/bin/vlc --intf dummy rtsp://$CAMERAUSER:$CAMERAPASSWORD@$CAMERAIP:$CAMERAPORT//$CAMERASTREAM --sout '#transcode{vcodec=MJPG,venc=ffmpeg{strict=1}}:standard{access=http{mime=multipart/x-mixed-replace;boundary=--7b3cc56e5f51db803f790dad720ed50a},mux=mpjpeg,dst=:8080/}'
